5b40f9818739d1996f55bcad388d2ee969580a62
KI Serge Laobien <sergelaobienk@gmail.com>
2020-03-24 19:04:04 +0000

    mis a jour des classe de calcul RPN

--- src/main/java/PGLP_4/PGLP4_2/MoteurRPN.java
+++ src/main/java/PGLP_4/PGLP4_2/MoteurRPN.java
@@ -2,4 +2,26 @@ package PGLP_4.PGLP4_2;
 
 public class MoteurRPN {
 
+	public void initMoteurRPN() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void apply(String saisie) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Enregistrer(double parseDouble) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Affiche() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
 }
--- src/main/java/PGLP_4/PGLP4_2/SaisieRPN.java
+++ src/main/java/PGLP_4/PGLP4_2/SaisieRPN.java
@@ -1,5 +1,72 @@
 package PGLP_4.PGLP4_2;
 
+import java.util.EmptyStackException;
+import java.util.Scanner;
+
 public class SaisieRPN {
+	
+	
+	private Scanner rep=new Scanner(System.in);
+	private MoteurRPN Moteur=new MoteurRPN();
+	private String saisie=new String();
+	
+	/**
+	 * Fonction d'interaction avec l'utilisateur et qui invoque le Moteur RPN
+	 * @throws ClasseException : Exception qui g�re la division par zero
+	 * @throws PilevideException : Exception qui g�re la pilevide
+	 * @throws ManqueOperandeException : Exception qui g�re le manque 
+	 * d'oprande pour effectuer un calcul
+	 * 
+	 */
+	public void reception()
+	{
+		Moteur.initMoteurRPN();
+		System.out.println("L'expression courante est: ");
+		do{
+			this.saisie=rep.nextLine();
+			if(verifisaisie(this.saisie)==false){
+				try {
+					Moteur.apply(this.saisie);
+				} catch (IllegalStateException e) {
+					// TODO: handle exception
+				}catch(EmptyStackException e) {
+					
+				}
+				
+			}else{
+			
+				Moteur.Enregistrer(Double.parseDouble(saisie));
+			}
+			
+			Moteur.Affiche();
+		
+		}while(true);
+		
+	}
+	
+	/**
+	 * Fonction qui permet de verifier si utilisateur a saisie une operande ou un operateur
+	 * et renvoi un booleen
+	 * @param val : valeur saisie par utilisateur
+	 * @return Renvoi Vrai si c'est une operande et Faux si non
+	 */
+	private boolean verifisaisie(String val){
+		try{
+			double test=Double.parseDouble(val);
+		}
+		catch(Exception e)
+		{
+			return false;
+		} 
+		return true;
+		
+	}
+
+
+
+
+	
+		
+	
 
 }
